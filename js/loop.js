223aa73832dcc641a3e0f81ae79bf7a96fc4673b
BogBok <55262975+BogBok@users.noreply.github.com>
2021-12-23 09:10:35 -0700

    Fixed Inf multiplier

--- js/loop.js
+++ js/loop.js
@@ -10,7 +10,7 @@ function gameLoop(that) {
     const infunlock = document.getElementsByClassName("inf-generator")[0];
     const infunlock2 = document.getElementsByClassName("inf-generator")[1];
     const infunlock3 = document.getElementsByClassName("inf-generator")[2];
-    if (that.player.money > 500000 || gemsBool === true) {
+    if (that.player.money > 25000 || gemsBool === true) {
         gemsunlock.style.visibility="visible"
         gemsunlock2.style.visibility="visible"
         gemsunlock3.style.visibility="visible"
@@ -22,7 +22,7 @@ function gameLoop(that) {
         gemsunlock3.style.visibility="hidden";
         gemsunlocked.style.visibility="hidden";
     }
-    if (that.player.money === Infinity || infBool === true) {
+    if (that.player.money === 1e16 || infBool === true) {
         infunlock.style.visibility="visible"
         infunlock2.style.visibility="visible"
         infunlock3.style.visibility="visible"
@@ -35,25 +35,29 @@ function gameLoop(that) {
         infunlocked.style.visibility="hidden";
     }
     let diff = (Date.now() - that.player.lastUpdate)/1000
-    that.player.marketing[0].mult = that.player.marketing[0].multincrease * ((that.player.gems / 1000) + 1)
-    that.player.marketing[1].mult = that.player.marketing[1].multincrease * ((that.player.gems / 1000) + 1)
-    that.player.marketing[2].mult = that.player.marketing[2].multincrease * ((that.player.gems / 1000) + 1)
+    that.player.marketing[0].mult = that.player.marketing[0].multincrease * ((that.player.gems / 10000) + 1)
+    that.player.marketing[1].mult = that.player.marketing[1].multincrease * ((that.player.gems / 10000) + 1)
+    that.player.marketing[2].mult = that.player.marketing[2].multincrease * ((that.player.gems / 10000) + 1)
 
-    that.player.artists[0].mult = that.player.artists[0].multincrease * ((that.player.infpoints / 1000) + 1)
-    that.player.artists[1].mult = that.player.artists[1].multincrease * ((that.player.infpoints / 1000) + 1)
-    that.player.artists[2].mult = that.player.artists[2].multincrease * ((that.player.infpoints / 1000) + 1)
+    that.player.artists[0].mult = that.player.artists[0].multincrease * ((that.player.infpoints / 10000) + 1)
+    that.player.artists[1].mult = that.player.artists[1].multincrease * ((that.player.infpoints / 10000) + 1)
+    that.player.artists[2].mult = that.player.artists[2].multincrease * ((that.player.infpoints / 10000) + 1)
+
+    that.player.inf[0].mult = that.player.inf[0].multincrease
+    that.player.inf[1].mult = that.player.inf[1].multincrease
+    that.player.inf[2].mult = that.player.inf[2].multincrease
 
     that.player.money += that.player.marketing[0].amount * that.player.marketing[0].mult * diff
     that.player.marketing[0].amount += (that.player.marketing[1].amount * that.player.marketing[1].mult * diff) / 100
-    that.player.marketing[1].amount += (that.player.marketing[2].amount * that.player.marketing[2].mult * diff) / 100
+    that.player.marketing[1].amount += (that.player.marketing[2].amount * that.player.marketing[2].mult * diff) / 1000
 
     that.player.gems += that.player.artists[0].amount * that.player.artists[0].mult * diff
     that.player.artists[0].amount += (that.player.artists[1].amount * that.player.marketing[1].mult * diff) / 100
-    that.player.artists[1].amount += (that.player.artists[2].amount * that.player.artists[2].mult * diff) / 100
+    that.player.artists[1].amount += (that.player.artists[2].amount * that.player.artists[2].mult * diff) / 1000
 
     that.player.infpoints += that.player.inf[0].amount * that.player.inf[0].mult * diff
     that.player.inf[0].amount += (that.player.inf[1].amount * that.player.inf[1].mult * diff) / 100
-    that.player.inf[1].amount += (that.player.inf[2].amount * that.player.inf[2].mult * diff) / 100
+    that.player.inf[1].amount += (that.player.inf[2].amount * that.player.inf[2].mult * diff) / 1000
 
     that.player.lastUpdate = Date.now()
     that.player.timer -= diff
